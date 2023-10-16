e5f59a4800e01d271bae80fb48c015c139c25c27
laboratorio <laboratorio@UPOAULA10715>
2023-10-16 08:30:21 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ar_cambios_taller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
