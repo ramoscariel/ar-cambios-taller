a7733b704911c9bef15959aad1ad4710b86cc208
laboratorio <laboratorio@UPOAULA10715>
2023-10-16 09:06:33 -0500

    Cambios a site.js del proyecto. Agregado funcion de boton.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function doSomething()
+{
+    document.getElementById("boton").innerHTML = "Ten un buen día.";
+}
